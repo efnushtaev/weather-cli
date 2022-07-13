07d62452cc1722b698b0c0000127661607dec0d9
efnushtaev <efnushtaev@gmail.com>
2022-07-13 21:24:53 +0300

    path and os realized

--- weather.js
+++ weather.js
@@ -2,12 +2,17 @@
 
 import { getArgs } from "./helpers/args.js";
 import { printHelp } from "./services/logService.js";
+import { saveKeyValue } from "./services/storageService.js";
 
 const initCLI = () => {
   const args = getArgs(process.argv);
 
   if (args.h) {
     printHelp();
+  }
+
+  if (args.t) {
+    saveKeyValue('token', args.t)
   }
 };
 
